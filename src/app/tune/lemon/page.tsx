1c679732f7710187bfe56c8d42ca6e173cce2180
Harsh <harshtanishq2002@gmail.com>
2024-06-17 21:33:23 +0530

    feat : explored folder structrues

---
+++ src/app/tune/lemon/page.tsx
@@ -0,0 +1,5 @@
+export default function Lemon(){
+    return (
+        <h1 className="text-3xl text-center">Lemon</h1>
+    )
+}
---
+++ src/app/tune/page.tsx
@@ -0,0 +1,5 @@
+export default function TunePage ( ) {
+    return(
+        <h1 className="text-3xl text-center mt-10">Tune</h1>
+    )
+}
