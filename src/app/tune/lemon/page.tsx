6041421ae6486d402f191bd804b3f9c65f84b771
Harsh <harshtanishq2002@gmail.com>
2024-06-18 17:36:51 +0530

    set-up

--- src/app/tune/lemon/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Lemon(){
-    return (
-        <h1 className="text-3xl text-center">Lemon</h1>
-    )
-}
--- src/app/tune/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function TunePage ( ) {
-    return(
-        <h1 className="text-3xl text-center mt-10">Tune</h1>
-    )
-}
