3140eb859320b31227adda7df9cb01113a3ea6b7
Harsh <harshtanishq2002@gmail.com>
2024-06-18 21:27:12 +0530

    feat: made Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <body>
+      <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.05]">
       <h1 className="text-3xl text-center">Find Music</h1>
+      </main>
     </body>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,8 +11,28 @@ function Navbar({ className }: { className?: string }) {
       className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
     >
       <Menu setActive={setActive}>
-        <Link> 
-          <MenuItem setActive={setActive} active={active} item="Home"></MenuItem>
+        <Link href={"/"}>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="Home"
+          ></MenuItem>
+        </Link>
+        <MenuItem setActive={setActive} active={active} item="Courses">
+          <div className="flex flex-col space-y-4 text-sm">
+            <HoveredLink href="/courses">All courses</HoveredLink>
+            <HoveredLink href="/courses">Basic Music Theory</HoveredLink>
+            <HoveredLink href="/courses">Advanced Composition</HoveredLink>
+            <HoveredLink href="/courses">SongWriting</HoveredLink>
+            <HoveredLink href="/courses">Music Production</HoveredLink>
+          </div>
+        </MenuItem>
+        <Link href={"/contact"}>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="Contact Us"
+          ></MenuItem>
         </Link>
       </Menu>
     </div>
