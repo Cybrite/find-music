d61ca47bdfefbd106cb2b6bf619c3d07e98d5cc5
Harsh <harshtanishq2002@gmail.com>
2024-06-20 12:19:43 +0530

    feat: SpotLight

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,15 @@
 import Link from "next/link";
+import React from "react";
+import { cn } from "@/utils/cn";
+import { Spotlight } from "./ui/Spotlight";
 
 function Hero() {
   return (
     <div className="h-auto md:h-[40rem] w-full rounded-sm flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0 text-center">
+      <Spotlight
+        className="-top-40 left-0 md:left-60 md:-top-20"
+        fill="white"
+      />
       <div className="p-4 relative z-10 w-full ">
         <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
           Master the art of Music
