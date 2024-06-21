28a0a544e3c5d44d81589825190f101f40e43f40
Harsh <harshtanishq2002@gmail.com>
2024-06-21 16:43:25 +0530

    feat: added Instructer Panel

---
+++ next.config.js
@@ -0,0 +1,6 @@
+// next.config.js
+module.exports = {
+    images: {
+      domains: ['images.unsplash.com', 'res.cloudinary.com'], // Add the hostname(s) you want to allow
+    },
+  };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Carousel } from "@/components/Carousel";
 import Featured from "@/components/Featured";
 import Hero from "@/components/Hero";
+import Instructors from "@/components/Instructors";
 import StickyCard from "@/components/StickyCard";
 import UpcomingWebinars from "@/components/UpcomingWebinars";
 
@@ -10,6 +11,7 @@ export default function Home() {
     <body>
       <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.05]">
         <Hero />
+        <Instructors/>
         <Featured />
         <StickyCard/>
         <Carousel/>
