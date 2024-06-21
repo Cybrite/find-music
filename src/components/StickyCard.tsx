df58f7776bb6b67ee480f492aead4479fd86df92
Harsh <harshtanishq2002@gmail.com>
2024-06-21 14:30:21 +0530

    chore: moved card content

--- src/components/StickyCard.tsx
+++ src/components/StickyCard.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { StickyScroll } from "./ui/sticky-scroll-reveal";
 import Image from "next/image";
+import { musicCourses } from "@/content/Card_content";
 
 // const content = [
 //     {
@@ -52,37 +53,6 @@ import Image from "next/image";
 //     },
 //   ];
 
-const musicCourses = [
-    {
-      title: "Introduction to Music Theory",
-      description: "Explore the cultural basis and historical context of diverse musical styles. Reflect on music notation as a problem-solving approach in musical expression using historically significant examples.",
-    },
-    {
-      title: "Guitar Fundamentals",
-      description: "Master the art of playing the guitar, from chords to fingerpicking techniques. Understand fretboard techniques, chord shapes, and strumming patterns.",
-    },
-    {
-      title: "Piano for Beginners",
-      description: "Learn keyboard basics, hand positions, and fingering techniques. Study note reading, scales, and basic music composition.",
-    },
-    {
-      title: "Electronic Music Production",
-      description: "Create electronic beats, melodies, and soundscapes using software tools. Explore synthesis, sampling, and arrangement techniques.",
-    },
-    {
-      title: "Music History and Appreciation",
-      description: "Trace the evolution of music styles from ancient to contemporary. Explore famous composers, cultural contexts, and musical movements.",
-    },
-    {
-      title: "Vocal Techniques",
-      description: "Improve breath control, vocal range, and expression. Study vocal health, resonance, and performance skills.",
-    },
-  ];
-  
-
-  
-  
-  
 function StickyCard() {
   return (
     <div className="p-10 ">
