f465877c80db43be68a1aee69f8c1134248cc41f
Harsh <harshtanishq2002@gmail.com>
2024-06-22 18:07:35 +0530

    chore: footer data

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,88 +1,53 @@
-
+import { quickLinks, socialLinks, about , contact} from "@/content/Footer";
 
 function Footer() {
-    return (
-      <footer className="bg-black text-gray-400 py-12">
-          <div className="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 px-4 sm:px-6 lg:px-8">
-          <div>
-            <h2 className="text-white text-lg font-semibold mb-4">About Us</h2>
-            <p className="mb-4">
-              Music School is a premier institution dedicated to teaching the art
-              and science of music. We nurture talent from the ground up,
-              fostering a vibrant community of musicians.
-            </p>
-          </div>
-          <div>
-            <h2 className="text-white text-lg font-semibold mb-4">Quick Links</h2>
-            <ul>
-              <li>
+  return (
+    <footer className="bg-black text-gray-400 py-12">
+      <div className="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 px-4 sm:px-6 lg:px-8">
+        <div>
+          <h2 className="text-white text-lg font-semibold mb-4">{about.title}</h2>
+          <p className="mb-4">
+            {about.des}
+          </p>
+        </div>
+        <div>
+          <h2 className="text-white text-lg font-semibold mb-4">Quick Links</h2>
+          <ul>
+            {quickLinks.map((links) => (
+              <li key={links.name}>
                 <a
-                  href="#"
+                  href={links.href}
                   className="hover:text-white transition-colors duration-300"
                 >
-                  Home
+                  {links.name}
                 </a>
               </li>
-              <li>
-                <a
-                  href="#"
-                  className="hover:text-white transition-colors duration-300"
-                >
-                  About
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="hover:text-white transition-colors duration-300"
-                >
-                  Courses
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="hover:text-white transition-colors duration-300"
-                >
-                  Contact
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <h2 className="text-white text-lg font-semibold mb-4">Follow Us</h2>
-            <div className="flex space-x-4">
-              <a
-                href="#"
-                className="hover:text-white transition-colors duration-300"
-              >
-                Facebook
+            ))}
+          </ul>
+        </div>
+
+        <div>
+          <h2 className="text-white text-lg font-semibold mb-4">Follow Us</h2>
+          <div className="flex space-x-4">
+            {socialLinks.map((social) => (
+              <a href={social.link} key={social.name} className="hover:text-white transition-colors duration-300">
+                {social.name}
               </a>
-              <a
-                href="#"
-                className="hover:text-white transition-colors duration-300"
-              >
-                Twitter
-              </a>
-              <a
-                href="#"
-                className="hover:text-white transition-colors duration-300"
-              >
-                Instagram
-              </a>
-            </div>
+            ))}
           </div>
-          <div>
-            <h2 className="text-white text-lg font-semibold mb-4">Contact Us</h2>
-            <p>Gorakhpur, UP, India</p>
-            
-            <p>Email: info@musicschool.com</p>
-            <p>Phone: (123) 456-7890</p>
-          </div>
-          </div>
-          <p className="text-center text-xs pt-8">© 2024 Music School. All rights reserved.</p>
-      </footer>
-    )
-  }
-  
-  export default Footer
+        </div>
+        <div>
+          <h2 className="text-white text-lg font-semibold mb-4">{contact.title}</h2>
+          <p> {contact.address} </p>
+          <p> {contact.email} </p>
+          <p>{contact.mobile}</p>
+        </div>
+      </div>
+      <p className="text-center text-xs pt-8">
+        © 2024 Music School. All rights reserved.
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/content/Footer.ts
@@ -0,0 +1,47 @@
+export const quickLinks = [
+  {
+    name: "Home",
+    href: "#",
+  },
+  {
+    name: "About",
+    href: "#",
+  },
+  {
+    name: "Courses",
+    href: "#",
+  },
+  {
+    name: "Blog",
+    href: "#",
+  },
+];
+
+export const socialLinks = [
+  {
+    name: "Facebook",
+    link: "#",
+  },
+  {
+    name: "Twitter",
+    link: "#",
+  },
+  {
+    name: "Instagram",
+    link: "#",
+  },
+];
+
+export const about = {
+  title: "About Us",
+  des: `Music School is a premier institution dedicated to teaching the art
+            and science of music. We nurture talent from the ground up,
+            fostering a vibrant community of musicians.`,
+};
+
+export const contact ={
+    title: 'Contact Us',
+    address : 'Gorakhpur, UP, India',
+    email : 'Email: info@musicschool.com',
+    mobile : 'Phone: (123) 456-7890',
+}
